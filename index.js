1ccb12ca6e4c73a8520d17ce9fa51164905074cd
obaikoko <jesseobinna7@gmail.com>
2024-01-21 22:37:04 +0100

    Learnable23 assignment 3

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const learnable23 = () => {
-    console.log('Learnable23 is a goal');
+const learnableProducts = {
+  laptop: { name: 'Laptop', price: 800 },
+  smartphone: { name: 'Smartphone', price: 500 },
+  headphones: { name: 'Headphones', price: 80 },
+};
+
+const shoppingCart = [];
+
+function addToCart(productKey, quantity) {
+  const product = learnableProducts[productKey];
+
+  if (product) {
+    const item = { ...product, quantity };
+    shoppingCart.push(item);
+    console.log(`${quantity} ${item.name}(s) added to the cart.`);
+  } else {
+    console.log('Product not found in the available products.');
+  }
 }
-learnable23()
+
+function displayCart() {
+  console.log('Shopping Cart:');
+  shoppingCart.forEach((item) => {
+    console.log(
+      `${item.name} - Quantity: ${item.quantity} - Price: $${
+        item.price * item.quantity
+      }`
+    );
+  });
+}
+
+addToCart('laptop', 2);
+addToCart('smartphone', 1);
+displayCart();
